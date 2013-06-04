97885a5bfa5d6023a83b2ade616c7904fb15190c
Sunil Pitkar <sunil_pitkar@HOTMAIL.COM>
2013-06-04 10:39:47 -0700

    Trying String as array

--- ArraysNStrings/ArraysNStrings/Program.cs
+++ ArraysNStrings/ArraysNStrings/Program.cs
@@ -13,36 +13,53 @@ namespace ArraysNStrings
             String revStr = null;
            
             str = ReadString();
-            revStr = ReverseString(str);
-            WriteString(revStr);
+            //revStr = ReverseString(str);
+
+            DisplayStringFromArray(str);
+
+
+
+           // WriteString(revStr);
+        }
+
+        private static void DisplayStringFromArray(string str)
+        {
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(str[i]);
+                //str[str.Length - 1] = "A";
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+            //return output;
         }
 
         private static string ReverseString(string str)
         {
-            int len = str.Length;
-            //Console.WriteLine(len.ToString());
-            int iStart = 0;
-            int iEnd = len - 1;
+            //int len = str.Length;
+            ////Console.WriteLine(len.ToString());
+            //int iStart = 0;
+            //int iEnd = len - 1;
 
-            //char temp;
-            //char[] charArray = str.ToCharArray();
-            //Console.WriteLine(charArray);
+            ////char temp;
+            ////char[] charArray = str.ToCharArray();
+            ////Console.WriteLine(charArray);
 
-            while(iEnd > iStart)
-            {
-                 Char temp = str[iStart];
-                Console.WriteLine(temp);
-                str[iStart] =str[iEnd];
-                str[iEnd] = temp;
-                iEnd--;
-                iStart++;
-                Console.WriteLine(str.ToString());
-            }
-            return str.ToString();
+            //while(iEnd > iStart)
+            //{
+            //     Char temp = str[iStart];
+            //    Console.WriteLine(temp);
+            //    str[iStart] =str[iEnd];
+            //    str[iEnd] = temp;
+            //    iEnd--;
+            //    iStart++;
+            //    Console.WriteLine(str.ToString());
+            //}
+            //return str.ToString();
 
             string output = ""; 
-            for (int i = str.Length - 1; i >= 0; i--)
-                output += str[i];
+            //for (int i = str.Length - 1; i >= 0; i--)
+            //    output += str[i];
                 return output;
         }
 

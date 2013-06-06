40e526782124a5c3bd9daaa62df6ff7e859d3db8
Sunil Pitkar <sunil_pitkar@HOTMAIL.COM>
2013-06-05 17:26:25 -0700

    Day2 some more programs
    anagram, repeated character

--- ArraysNStrings/ArraysNStrings/Program.cs
+++ ArraysNStrings/ArraysNStrings/Program.cs
@@ -10,29 +10,110 @@ namespace ArraysNStrings
         public static void Main(string[] args)
         {
             String str = null;
-            String revStr = null;
+            String newStr = null;
            
-            str = ReadString();
-            //revStr = ReverseString(str);
-
-            DisplayStringFromArray(str);
-
-
-
-           // WriteString(revStr);
+           str = ReadString();
+           //revStr = ReverseString(str);
+           //revStr = ReverseStringByWords(str);
+           //FindFirstRepeatedCharacter(str);
+           //IsStringAnAnagram(str);
+           IsStringAPalndrome(str);
+            
+            WriteString(newStr);
         }
 
-        private static void DisplayStringFromArray(string str)
+        private static void IsStringAPalndrome(string str)
         {
-            for (int i = str.Length - 1; i >= 0; i--)
-            {
-                Console.WriteLine(str[i]);
-                //str[str.Length - 1] = "A";
-            }
-            Console.WriteLine(str);
-            Console.ReadKey();
-            //return output;
+            throw new NotImplementedException();
         }
+
+        private static void IsStringAnAnagram(string str)
+        {
+            ///Guard Clause
+            string str1 = str.ToUpper();
+            string str2 = ReadString();
+            
+            //convert string to uppe
+            str2 = str2.ToUpper();
+            Console.WriteLine(str1 + " " + str2);
+                if (str1.Length != str2.Length || str1.Length == 0)
+            {
+                Console.WriteLine("Strings are not Anangrams");
+                return;
+            }
+
+            
+           //create lookup table
+            int[] A = new int[128];
+            //for (int i = 0; i < 128; i++) { A[i] = 0; }
+            for (int i = 0; i <str1.Length; i++)
+            {
+                A[str1[i]]++;
+                A[str2[i]]--;
+            }
+                
+            for(int i=0; i<128;i++)
+                {
+                    //Console.WriteLine(i + A[i]);  
+                if(A[i] !=0) 
+                  {
+                  Console.WriteLine("Not Anagrams");
+                  return;
+                  }
+                }
+                    Console.WriteLine("ANAGRAMS");
+                
+            //next check 128 vs 256 bytes
+            //all combinations of a string are anagrams.
+            // check without changing case.
+
+            
+        }
+
+        private static void FindFirstRepeatedCharacter(string str)
+        {
+           //guard clauses
+            if (str.Length == 0 || str.Length == 1)
+            {
+                Console.WriteLine("String empty or single character");
+                return;
+            }
+
+            //createlookup table & check values(for kicks)
+            bool[] lookUpTable= new bool[128];
+            for(int i = 0;i<str.Length;i++)
+            {
+                if(lookUpTable[str[i]])
+                {
+                Console.WriteLine("Repeats");
+                }
+                else
+                {
+                    
+                    lookUpTable[str[i]]=true; 
+                }
+
+            }
+
+
+
+            //try find repeated character
+            //other questions are type of encoding 128 vs 256 and chinese characters, also questiosn about case sensitive characters
+
+
+        }
+
+        //private static void DisplayStringFromArray(string str)
+        //{
+        //    for (int i = str.Length - 1; i >= 0; i--)
+        //    {
+        //        Console.WriteLine(str[i]);
+        //        //str[str.Length - 1] = "A";
+        //    }
+        //    Console.WriteLine(str);
+        //    Console.ReadKey();
+        //    //return output;
+        //}
 
         private static string ReverseString(string str)
         {
@@ -57,10 +138,12 @@ namespace ArraysNStrings
             //}
             //return str.ToString();
 
-            string output = ""; 
-            //for (int i = str.Length - 1; i >= 0; i--)
-            //    output += str[i];
-                return output;
+            string output = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                output += str[i];
+            }
+            return output;
         }
 
         private static void WriteString(String str)

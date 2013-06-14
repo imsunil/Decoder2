5ebd9e5a20e25ee2da33d966ded61f137691cf22
Sunil Pitkar <sunil_pitkar@hotmail.com>
2013-06-14 01:13:20 -0700

    finished graph class

---
+++ My Graph/My Graph/AGraph.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Graph
+{
+    class AGraph
+    {
+        private List<ANode> m_Nodes = new List<ANode>;
+        private char m_startNode;
+
+        void AddNode(ANode node)
+        ANode GetNode(char ID)
+        char GetStartNode()
+        void SetStartNode(char cStart)
+            void BreadthFirst(char cStart)
+        
+    }
+}
--- My Graph/My Graph/ANode.cs
+++ My Graph/My Graph/ANode.cs
@@ -13,31 +13,44 @@ namespace My_Graph
         private bool m_visited;
         private List<AEdge> m_Edges = new List<AEdge>();
         
-        public ANode(char ID, int dada)
-        { 
-        
+        public ANode(char ID, int data)
+        {
+            this.m_ID = ID;
+            this.data = data;
+            m_visited = false;
         }
 
         public void AddEdge(char cEnd, int weight)
         {
-        AEdge edge = new AEdge((m_ID,cEnd,weight);
-        m_Edges.Add(edge)
+        AEdge edge = new AEdge(m_ID,cEnd,weight);
+        m_Edges.Add(edge);
         }
 
         public void DeleteEdge(char cEnd)
         {
-        //AEdge edge = new AEdge((m_ID,cEnd,weight);
-        
-            for(int i=0; i<m_Edges.Count();i++)
+            //AEdge edge = new AEdge((m_ID,cEnd,weight);
+
+            for (int i = 0; i < m_Edges.Count(); i++)
             {
-                AEdge edge = m_Edges(i);
-                if (edge.getEnd == cEnd)
+                AEdge edge = m_Edges[i];
+                if (edge.getEnd() == cEnd)
                 {
-                    m_Edges.erase(m_Edges.begin() + i);
+                    m_Edges.RemoveAt(i);
                     break;
                 }
 
             }
+
+        }
+
+        public List<char> GetNeighbours()
+        {
+            List<char> neighbours = new List<char>;
+            for (int i = 0; i < m_Edges.Count(); i++)
+            {
+                neighbours.Add(m_Edges[i].getEnd());
+            }
+            return neighbours;
         }
 
     }
